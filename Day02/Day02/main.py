eff3e0ce0182928e6f29c16079bc95cfb359709c
Aryan patil <aryanpatil8292@gmail.com>
2026-06-08 19:09:53 +0530

    Create main.py

---
+++ Day02/Day02/main.py
@@ -0,0 +1,25 @@
+# Day 2 - Variables and User Input
+
+# Variables
+name = "Aryan"
+age = 21
+height = 5.8
+is_student = True
+
+# Display Variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print("\n--- User Information ---")
+
+# User Input
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+
+# Display User Information
+print("\nHello,", user_name + "!")
+print("You are", user_age, "years old.")
+
+print("\nDay 2 Challenge Completed!")
