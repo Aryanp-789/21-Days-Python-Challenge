14301bd8f38141024e336eaa528a41987fad49cd
Aryan patil <aryanpatil8292@gmail.com>
2026-06-11 10:35:59 +0530

    Create day5.py

---
+++ Day-5/day5.py
@@ -0,0 +1,21 @@
+# Print numbers from 1 to 10
+
+for i in range(1, 11):
+    print(i)
+
+
+# Multiplication Table
+
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+
+# While Loop Example
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
